32e83a707b0120de903179c8a147e54ec4f672fd
Jonathan Diehl <jonathan.diehl@inform-software.com>
2017-07-08 17:17:48 +0200

    added stringToDate

---
+++ src/date.ts
@@ -0,0 +1,17 @@
+function unitFactor(unit: string): number {
+  switch (unit) {
+  case 's': return 1000
+  case 'm': return 60000
+  case 'h': return 3600000
+  case 'd': return 86400000
+  case 'w': return 604800000
+  case 'y': return 31536000000
+  default: throw new Error('Invalid Unit')
+  }
+}
+
+export function stringToDuration(x: string) {
+  const unit = x[x.length - 1]
+  const d = parseInt(x, 10)
+  return d * unitFactor(unit)
+}
